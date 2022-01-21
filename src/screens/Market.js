fe40e4d42356ba4ef4a28adaa718fb569a8ab985
yao-charlie <charlieyao@gmail.com>
2022-01-20 23:22:08 -0800

    copy revised, graphics updated, general rf

--- src/screens/Market.js
+++ src/screens/Market.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Market.css'
+import SVG_Market from '../graphics/market.svg';
+
 
 function Market() {
     return (
@@ -7,12 +9,12 @@ function Market() {
             <div className="market-container">
                 <div className="market-title">
                     <h2>Market</h2>
+                    <div className = "market-image-block">
+                        <img className = "market-image" src={SVG_Market}/>
+                    </div>
                     <p>
-                    With a wide application across global commercial and industrial sectors, MAPS will focus the initial launch targeting capital-intensive businesses in the Toronto-Windsor corridor.  Particularly, the focus will be on automotive parts and food and beverage manufacturers which representing over 3,700 companies in Ontario. The MPP efficiently addresses these business’ requirements for a solution that averts the rising cost of grid electricity to protect their competitiveness, combats greenhouse gas emissions, and has the reliability and rapid response to avoid costly grid outages and meet fluctuating load requirements. The nature of these business’ electricity consumption profile, investment decision-making behavior, and MAPS industry familiarity were amongst additional factors considered in the selection of the optimal initial target market.     
+                    With a broad application across global industrial, commercial, and grid customers, MAPS will focus the initial launch targeting Ontario’s automotive parts and food and beverage manufacturers representing over 3,700 companies. The rapidly rising electricity cost and international competition, customer billing structure from grid operators, current market existence for peak-shaving abatement, and a favorable regulatory environment promoting green energy were amongst the factors considered in choosing Ontario as the launch market.  The MPP efficiently addresses these businesses' broad requirements for an onsite power solution. First, the solution mitigates the rising cost of grid electricity to protect competitiveness. Second, it combats greenhouse gas emissions to assist these businesses in meeting their emission targets. Lastly, provides reliability and rapid response to avoid costly grid outages and fulfill fluctuating load requirements.
                     </p>
-                </div>
-                <div className = "market-image-block">
-                    <img className = "market-image" src="https://source.unsplash.com/random/250x250"/>
                 </div>
 
             </div>
